e389306a27c1f415744a1be2e1a2a99bbe50c00a
mau <2991404947@qq.com>
2024-12-21 16:23:05 +0800

    add feature compute

--- antifraud-front/src/main/java/com/hsbc/web/controller/AntifraudController.java
+++ antifraud-front/src/main/java/com/hsbc/web/controller/AntifraudController.java
@@ -1,7 +1,7 @@
 package com.hsbc.web.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.hsbc.response.AntifraudResponse;
 import com.hsbc.service.AntifraudService;
 import com.hsbc.util.AntifraudUtil;
--- antifraud-service/src/main/java/com/hsbc/util/AntifraudUtil.java
+++ antifraud-service/src/main/java/com/hsbc/util/AntifraudUtil.java
@@ -1,7 +1,7 @@
 package com.hsbc.util;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.hsbc.constants.CodeEnum;
 import com.hsbc.response.AntifraudResponse;
 import lombok.extern.slf4j.Slf4j;
