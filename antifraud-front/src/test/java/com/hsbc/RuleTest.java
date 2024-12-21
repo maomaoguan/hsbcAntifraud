2c4e138031dcb4f900b0465352c6c461c3ccacc7
mau <2991404947@qq.com>
2024-12-21 12:14:47 +0800

    add data and feature

--- antifraud-front/src/test/java/com/hsbc/RuleTest.java
+++ antifraud-front/src/test/java/com/hsbc/RuleTest.java
@@ -7,6 +7,8 @@ import com.hsbc.service.RuleService;
 import com.hsbc.util.FileUtil;
 import com.hsbc.vo.RuleVo;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +23,12 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -64,6 +72,51 @@ public class RuleTest {
 
         Assert.assertNotNull(ruleVo);
         Assert.assertEquals(ruleVo.getStatus(), StatusEnum.ONLINE.getStatus());
+    }
+
+    private String genLine(Map<String, Object> featureParameters) {
+        StringBuilder builder = new StringBuilder();
+
+        int i = 0;
+        for (Map.Entry<String, Object> featureParameter : featureParameters.entrySet()) {
+            builder.append(featureParameter.getValue());
+
+            if (i < featureParameters.size() - 1) {
+                builder.append(',');
+            }
+
+            i++;
+        }
+
+        return builder.toString();
+    }
+
+    @Test
+    public void testDataGen() throws Exception {
+        Map<String, Object> featureParameters = new HashMap<>();
+
+        featureParameters.put("fUserId", "43200601010002xxx");
+        featureParameters.put("fMobile", "1332123");
+        featureParameters.put("fAmount", 10);
+        featureParameters.put("fEventTime", System.currentTimeMillis());
+        featureParameters.put("fStatus", 0);
+
+        for (int i = 0; i < 3; i++) {
+            featureParameters.put("fUserId", "43200601010002xxx" + RandomUtils.nextInt(0, 3));
+
+            List<String> lines = new ArrayList<>();
+
+            for (int j = 0; j < 100; j++) {
+                featureParameters.put("fAmount", RandomUtils.nextInt(0, 10));
+                featureParameters.put("fEventTime", System.currentTimeMillis());
+
+                String line = this.genLine(featureParameters);
+                lines.add(line);
+            }
+
+            FileUtils.writeLines(new File("/Users/maomao/Documents/workspace/data/antifraud/" + i), lines);
+        }
+
     }
 
 }
--- antifraud-service/src/main/java/com/hsbc/vo/FeatureVo.java
+++ antifraud-service/src/main/java/com/hsbc/vo/FeatureVo.java
@@ -1,4 +1,30 @@
 package com.hsbc.vo;
 
+/**
+ * this is a representation of all types of features, feature major fields are described while internal logic are encapsulated in transparency manner
+ */
 public class FeatureVo {
+    /**
+     * the feature name
+     */
+    private String name;
+    /**
+     * the feature name in renderable form, like in CH character
+     */
+    private String displayName;
+    /**
+     * feature status, like online, drafted, tested, and etc
+     */
+    private String status;
+
+    private String funcType;
+
+    private String fieldName;
+
+    private String filter;
+
+    private int period;
+
+
+
 }
